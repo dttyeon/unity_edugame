bb9baa5f6734ea41e5606b5ae60bc379c13d745b
LAPTOP-EDMSON1E\user <cay906@naver.com>
2017-10-18 15:36:41 +0900

    merse

---
+++ Assets/02.Script/Ball/MoveBall.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+    public Vector2 minPos;
+    public Vector2 maxPos;
+
+    private Transform ballTf;
+    private Vector3 ballPos;
+    private Vector2 moveValue;
+    private float moveSpeed = 0.15f;
+
+    private void Awake()
+    {
+        ballTf = transform;
+        ballPos = ballTf.position;
+        moveValue = Vector2.one;
+    }
+    void FixedUpdate()
+    {
+        ballPos.x += moveSpeed * moveValue.x;
+        ballPos.y += moveSpeed * moveValue.y;
+        if (ballPos.x < minPos.x)
+        {
+            moveValue.x *= -1;
+            ballPos.x += minPos.x - ballPos.x;
+        }else if (ballPos.x > maxPos.x)
+        {
+            moveValue.x *= -1;
+            ballPos.x += maxPos.x - ballPos.x;
+        }
+
+        if (ballPos.y < minPos.y)
+        {
+            moveValue.y *= -1;
+            ballPos.y += minPos.y - ballPos.y;
+        }
+        else if (ballPos.y > maxPos.y)
+        {
+            moveValue.y *= -1;
+            ballPos.y += maxPos.y - ballPos.y;
+        }
+
+        ballTf.position = ballPos;
+    }
+
+    public void TurnTheBall(int type)
+    {
+        if(type == 1)
+        {
+            moveValue *= -1;
+        }
+        else if (type == 2)
+        {
+            moveValue *= -1;
+        }
+    }
+}
---
+++ Assets/02.Script/Block/BlockInfo.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockInfo : MonoBehaviour {
+    int healthPoint;
+    float xHalfSize;
+    float yHalfSize;
+    Vector3 thisBlockPos;
+
+    Vector3 tempBallPos;
+    Vector3 tempBallScale;
+    static float ballRadius = 1.2f;
+
+    void Awake()
+    {
+        thisBlockPos = transform.position;
+
+        BoxCollider2D tempCol = gameObject.GetComponent<BoxCollider2D>();
+        xHalfSize = transform.localScale.x * (tempCol.size.x * 0.5f);
+        yHalfSize = transform.localScale.y * (tempCol.size.y * 0.5f);
+        tempCol = null;
+    }
+
+    float shortestDist;
+    int shortestSide = 0;
+    float tempDist;
+
+    void CheckCollisionSide()
+    {
+        shortestDist = Mathf.Abs(tempBallPos.x + (tempBallScale.x * ballRadius) - thisBlockPos.x - xHalfSize);
+        shortestSide = 1;
+        tempDist = Mathf.Abs(tempBallPos.x - (tempBallScale.x * ballRadius) - thisBlockPos.x + xHalfSize);
+        if (shortestDist > tempDist)
+        {
+            shortestDist = tempDist;
+            shortestSide = 2;
+        }
+        tempDist = Mathf.Abs(tempBallPos.y - (tempBallScale.y * ballRadius) - thisBlockPos.y + xHalfSize);
+        if (shortestDist > tempDist)
+        {
+            shortestDist = tempDist;
+            shortestSide = 3;
+        }
+        tempDist = Mathf.Abs(tempBallPos.y + (tempBallScale.y * ballRadius) - thisBlockPos.y - xHalfSize);
+        if (shortestDist > tempDist)
+        {
+            shortestDist = tempDist;
+            shortestSide = 4;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("충돌");
+        tempBallPos = col.transform.position;
+        tempBallScale = col.transform.localScale;
+
+        CheckCollisionSide();
+
+        if (shortestSide < 3)
+        {
+            col.gameObject.GetComponent<MoveBall>().TurnTheBall(2);
+        }
+        else
+        {
+            col.gameObject.GetComponent<MoveBall>().TurnTheBall(1);
+        }
+        shortestSide = 0;
+    }
+
+}
