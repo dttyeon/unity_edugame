6a48d1fb7080759f3c125ef43e6b173208b34d90
LAPTOP-EDMSON1E\user <cay906@naver.com>
2017-10-18 15:30:09 +0900

    움직이기

---
+++ Assets/02.Script/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveControl();
+	}
+
+    void moveControl() {
+
+        float distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float distanceY = Input.GetAxis("Vertical")*moveSpeed * Time.deltaTime;
+        this.gameObject.transform.Translate(distanceX, distanceY, 0);
+        
+   
+
+    }
+}
